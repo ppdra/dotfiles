aa8220d34845bf648301de4ed3244e3d202a70af
ppdra <pepedra03@gmail.com>
2024-04-11 02:07:42 -0300

    auto sync obsidian

---
+++ scripts/sync_obsidian.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+
+base_dir=~/repos/obsidian
+
+cd $base_dir
+
+last_remote_commit=$(git ls-remote origin HEAD | awk '{print $1}')
+
+
+function sync ()
+{
+  if [[ -z $(git log | grep $1) ]]; then
+        
+    echo "Local repo behind (git pull)" >> $base_dir/sync_logs.md
+    date >> $base_dir/sync_logs.md
+    echo ""
+
+    git pull 
+
+  elif [[ -n $(git diff) ]]; then
+    
+    echo "Remote repo behind (git push)" >> $base_dir/sync_logs.md
+    date >> $base_dir/sync_logs.md
+    echo ""
+   
+   # ira verificar se tem alguma diff, caso tenha ira fazer sync para o remoto
+    git add .
+    git commit -m "SCRIPT AUTO SYNC"
+    git push
+
+  fi
+}
+
+
+sync $last_remote_commit
