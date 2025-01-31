e9fd46cfeaed62fceb5826ff721507a1a04bd42e
ppdra <pepedra03@gmail.com>
2025-01-31 20:34:00 -0300

    {comment}

--- scripts/codecommit.py
+++ scripts/codecommit.py
@@ -1,7 +1,29 @@
+import re
+import subprocess
 import os
 
+def run_alias_and_extract(alias_command):
+    command = f"zsh -i -c '{alias_command}'"
+    result = subprocess.run(command, shell=True, text=True, capture_output=True)
 
-def main():
-    os.system('gccs')
-    
-main()
+    if result.returncode == 0:
+        output = result.stdout
+        matches = re.search(r"```(.*?)```", output, re.DOTALL).group(1)
+        return matches 
+    else:
+        print("Erro ao executar o comando:")
+        print(result.stderr)
+        return None
+
+comment = run_alias_and_extract("gccs")
+
+print(comment)
+
+print("-------------------------------------")
+resposta = input("Commit? (y/n): ")
+if resposta.lower() == "y":
+    os.system("git add .")
+    os.system("git commit -m {comment}")
+    os.system("git push")
+else:
+    print("Commit cancelado.")
